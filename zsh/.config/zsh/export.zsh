54aa0123ebe95a58a76edf0bcefdd4b77d6d5795
Rachel Shaw <rachel@beijaflor.es>
2019-02-21 23:42:28 -0500

    mpd

--- zsh/.config/zsh/export.zsh
+++ zsh/.config/zsh/export.zsh
@@ -58,7 +58,8 @@ export PATH=$BINARIES:$(ruby -e 'print Gem.user_dir')/bin:$PATH
 export PYTHONPATH=~/dev:$PYTHONPATH
 
 # MPD stuff
-export MPD_HOST=$(cat ${XDG_DATA_HOME}/mpc/MPC_HOST.secret)
+export MPD_PASS=$(cat ${XDG_DATA_HOME}/mpd/password)
+export MPD_HOST=${MPD_PASS}@10.0.0.251
 
 # Qt4/Qt5/KDE/GTK theme stuff
 export QT_QPA_PLATFORMTHEME=qt5ct
